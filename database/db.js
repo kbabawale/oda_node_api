abcb260265cef6905fc0169752920b67b6078091
Kolapo 'Kay' Babawale <kolapobabawale@gmail.com>
2022-09-22 22:28:45 +0100

    Delete db.js

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("oda", "oda", "@Sun123", {
-   host: "localhost",
-   dialect: "postgres",
-   timezone: "Africa/Lagos"
-});
-
-module.exports = sequelize;
